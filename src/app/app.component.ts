25c98279d57c358df1755b0020306e68a268e3eb
Henrik Westerholm <henrikwesterholm@Henriks-MacBook-Air.local>
2024-11-28 00:51:28 +0200

    Initialize project

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, ViewContainerRef } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NighthawkBootstrapService } from 'ngx-f117';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  constructor(
+    private readonly vcr: ViewContainerRef,
+    private readonly nighthawk: NighthawkBootstrapService
+  ) {}
+
+  public ngAfterViewInit(): void {
+    this.nighthawk.setVcrInstance(this.vcr);
+    this.nighthawk.loadApplication({
+      timezone: 'guess',
+      databaseTimezone: 'UTC',
+      pageLoaderEnabled: true,
+      pageLoaderSize: 54,
+      pageLoaderType: 'tube-spinner',
+      pageLoaderCustomImagePath: false,
+      routeLoaderEnabled: false,
+      minimumLoaderTime: 1000,
+      manualLoader: false,
+      fonts: [],
+    });
+  }
+}
---
+++ src/app/app.config.ts
@@ -0,0 +1,34 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  InMemoryScrollingFeature,
+  InMemoryScrollingOptions,
+  provideRouter,
+  withInMemoryScrolling,
+  withViewTransitions,
+} from '@angular/router';
+
+import { routes } from './app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
+};
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature =
+  withInMemoryScrolling(scrollConfig);
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, inMemoryScrollingFeature, withViewTransitions()),
+    provideClientHydration(withEventReplay()),
+    provideAnimations(),
+    provideHttpClient(withFetch()),
+  ],
+};
